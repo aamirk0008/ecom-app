73524c37f875c9f819b2f63de38ae9589da17b5f
aamirk0008 <113338901+aamirk0008@users.noreply.github.com>
2025-11-13 02:12:20 +0530

    product services

--- controller/product.controller.js
+++ controller/product.controller.js
@@ -33,8 +33,101 @@ const fetchProducts = async (req, res) => {
     }
 }
 
+const updateProduct = async (req, res) => {
+    try {
+        const id = req.params.id
+
+        const updates = req.body
+
+        const updatedPro = await Product.findByIdAndUpdate(
+            id,
+            updates,
+            {
+                new: true
+            }
+        )
+
+        if (!updatedPro) {
+            return res.status(404).json({
+                message: 'Product not found'
+            })
+        }
+
+        res.status(200).json({
+            message: 'Product updated successfully',
+            product: updatedPro
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: 'Internal server error'
+        })
+    }
+}
+ 
+
+const deleteProduct = async (req, res) => {
+    try {
+        const id = req.params.id
+
+        const deleteProduct = await Product.findByIdAndDelete(id)
+
+        if (!deleteProduct) {
+            return res.status(404).json({
+                message: 'Product not found'
+            })
+        }
+        res.status(200).json({
+            message: 'Product deleted successfully',
+            deletedProduct: deleteProduct
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: 'Internal server error'
+        })
+    }
+}
+ 
+const searchProducts = async (req, res) => {
+    try {
+        const {query} = req.query
+
+        const searchResults = await Product.find(
+            {
+                $or: [
+                    {
+                        name: {
+                            $regex: query
+                        }
+                    },
+                    {
+                        description: {
+                            $regex: query
+                        }
+                    }
+                ]
+            }
+        )
+
+        if (searchResults.length === 0) {
+            return res.status(404).json({
+                message: 'No products found matching the query'
+            })
+        }
+        res.status(200).json({
+            message: 'Products fetched successfully',
+            products: searchResults
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: 'Internal server error'
+        })
+    }
+}
 
 module.exports = {
     createProduct,
-    fetchProducts
+    fetchProducts,
+    updateProduct,
+    deleteProduct,
+    searchProducts
 }
--- view/product.view.js
+++ view/product.view.js
@@ -1,11 +1,17 @@
 const express = require('express');
-const { createProduct, fetchProducts } = require('../controller/product.controller');
+const { createProduct, fetchProducts, updateProduct, deleteProduct, searchProducts } = require('../controller/product.controller');
 const router = express.Router();
 
 
 router.post('/createProduct', createProduct)
 
 router.get('/fetchProducts', fetchProducts)
+
+router.put('/updateProduct/:id', updateProduct)
+
+router.delete('/deleteProduct/:id', deleteProduct)
+
+router.get('/search', searchProducts)
 
 
 
